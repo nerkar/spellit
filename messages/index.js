4f2e18229799f62582ca72154c535f10d1d83209
nerkar <nerkarpraveen@gmail.com>
2017-04-17 12:16:28 +0530

    added spellit code

--- messages/index.js
+++ messages/index.js
@@ -1,6 +1,10 @@
 "use strict";
 var builder = require("botbuilder");
 var botbuilder_azure = require("botbuilder-azure");
+var dictionary = require('dictionary-en-us');
+var nspell = require('nspell'); 
+var wordnet = require('wordnet');
+var outputStr = "";
 
 var useEmulator = (process.env.NODE_ENV == 'development');
 
@@ -14,7 +18,10 @@ var connector = useEmulator ? new builder.ChatConnector() : new botbuilder_azure
 var bot = new builder.UniversalBot(connector);
 
 bot.dialog('/', function (session) {
-    session.send('You said ' + session.message.text);
+  checkSpell(session.message.text, function (outputStr) {
+    session.send(' : ' + outputStr);
+  })
+    //session.send('You said ' + session.message.text);
 });
 
 if (useEmulator) {
@@ -26,4 +33,46 @@ if (useEmulator) {
     server.post('/api/messages', connector.listen());    
 } else {
     module.exports = { default: connector.listen() }
+}
+
+
+function checkSpell(word, callback) {
+  var result = "";
+  dictionary(function (err, dict) {
+    if (err) {
+      callback('Oops! something went wrong...')
+    }
+    var spell = nspell(dict);
+    if (spell.correct(word) === true) {
+
+      //result = 'Hey here is the meaning of word with few sentence' + lookupWord(word);
+      lookupWord(word, function (meaning) {
+        callback(' : ' + meaning)
+      })
+    }
+    else {
+      callback('I think there is some thing wrong in spelling -- here are few Suggestions : \n' + spell.suggest(word));
+    }
+
+  });
+}
+
+function lookupWord(word, callback) {
+  var glossary = "";
+  wordnet.lookup(word, function (err, definitions) {
+    if (err) {
+      callback('no definition found for spesified word')
+    }
+    else {
+      if (definitions) {
+        definitions.forEach(function (definition) {
+          //console.log('  words: %s ', JSON.stringify(definition ));                      
+          //console.log('  %s', definition.glossary);
+          //glossary = definition.glossary;
+          callback(definition.glossary);
+        });
+      }
+    }
+
+  });
 }
